3caccd566b5ca5ad37803f045caa5ef4103cb7a5
unknown <Tobias.Pleyer@GAR-WS-LAMA11.ad.physik.uni-muenchen.de>
2014-08-14 14:09:23 +0200

    2014/08/14  2:09 pm.

---
+++ compensation_minFuncForBruteForce_no_global2.m
@@ -0,0 +1,17 @@
+function val=compensation_minFuncForBruteForce_no_global2(polynomial,w0,w,I,p,Int_F)
+
+    P          = polyval(polynomial,w-w0);
+    % In order to increase the sampling rate in the fourier domain, it is
+    % necessary to add more information in the spectral domain.
+    % Since we cannot invent a phase in this spectral region, a common
+    % trick is to add zeros.
+    D          = p - P;
+    L          = length(D);
+    [w,I,D,l]  = compensation_extendPhaseByZeros(w,I,D,L);
+    Sk_cplx    = sqrt(I) .* exp(1i*D);
+    [t,E]      = Speck_Fourier(l.*1e-9,Sk_cplx);
+    t          = t * 1e15;
+    Int        = abs(trapz(t,abs(E).^2));
+    E          = E .* sqrt(Int_F ./ Int);
+    val        = 1-max(abs(E).^2);
+end
--- compensation_runTestSuite.m
+++ compensation_runTestSuite.m
@@ -41,6 +41,7 @@ figure(figNum)
     set(get(AX(1),'Ylabel'),'String','arbitrary units')
     set(get(AX(2),'Ylabel'),'String','[rad]')
     xlabel('omega [1/fs]')
+    title('Test 0: The pulse with intensity and spectrum we work with')
 
 % TEST 1: See how our Fourier transformation performs.
 fprintf('TEST 1: Fourier Trafo\n')
@@ -65,7 +66,7 @@ figure(figNum)
     xlabel('time [fs]')
     ylabel('relative units')
     legend('Fourier limit','Real pulse GDD 5e4','Real pulse GDD 8e4','Real pulse GDD 11e4')
-    title('Observation of the effects of different GDDs on the pulse')
+    title('Test 1: Observation of the effects of different GDDs on the pulse')
     
 % TEST 2: See if adding zeros increases our sample rate.
 fprintf('TEST 2: Increase sample rate\n')
@@ -98,7 +99,7 @@ figure(figNum)
     xlabel('time [fs]')
     ylabel('relative units')
     legend('Fourier limit 1xL','Real pulse 1xL','Fourier limit 2xL','Real pulse 2xL','Fourier limit 3xL','Real pulse 3xL')
-    title('Observation of the increase in peak sampling rate by adding zeros')
+    title('Test 2: Observation of the increase in peak sampling rate by adding zeros')
     
 % TEST 3: See if our brute force ansatz works with our simple sample phase
 fprintf('TEST 3: Approximation of the phase curve via brute force\n')
@@ -132,6 +133,7 @@ end
 figure(figNum)
     figNum = figNum + 1;
     plot(V)
+    title('Test 3: Calcultated values of our minimization function 1D case')
 
 fprintf('      > 2D\n')
 
@@ -146,7 +148,7 @@ figure(figNum)
     set(get(AX(1),'Ylabel'),'String','arbitrary units')
     set(get(AX(2),'Ylabel'),'String','[rad]')
     xlabel('omega [1/fs]')
-    title('New phase with p = 5000*(w-w0).^3+80000*(w-w0).^2')
+    title('Test 3: New phase with p = 5000*(w-w0).^3+80000*(w-w0).^2')
 
 % Reserve arrays for storage
 min_a = 5e5;
@@ -165,6 +167,146 @@ for a=A
         % Calculate the value
         val = compensation_minFuncForBruteForce_no_global(polynomial,w,I,p,Int_F);
         V(N) = val;
+%         if mod(N,1000)==0
+%             fprintf('N: %d\n',N)
+%         end
+        N = N + 1;
+    end
+end
+
+figure(figNum)
+    figNum = figNum + 1;
+    h = surf(A,B,V);
+    % Necessary, or the lines of the grid will overlay the colors
+    set(h,'LineStyle','none')
+    title('Test 3: Calcultated values of our minimization function 2D case')
+
+% TEST 4: Apply the results on the measured phase
+fprintf('TEST 4: Apply the results on the measured phase\n')
+
+[t_Et,I_Et,p_Et,l_Sk,I_Sk,p_Sk]       = compensation_loadData();
+
+[Int_F,t_F,Ek_F]                      = compensation_calcFourierlimit(I_Sk,l_Sk);
+
+[l0,w0,w_spacing,w_Sk,I_Sk,p_Sk,l_Sk] = compensation_makeOmegaEqualSpaced(l_Sk,I_Sk,p_Sk);
+
+[B,A]                                 = compensation_makeFilter(w_Sk);
+
+filtered_p_Sk                         = filtfilt(B,A,p_Sk);
+
+[fit_w_Sk,fit_p_Sk,fit_I_Sk,fit_l_Sk] = compensation_findRangeOfInterest(I_Sk,w_Sk,l_Sk,p_Sk);
+
+[D1,D2,D3]                            = compensation_calcDevs(filtered_p_Sk,w_Sk,B,A);
+
+
+figure(figNum)
+    figNum = figNum + 1;
+    [AX,H1,H2] = plotyy(w_Sk,I_Sk,w_Sk,p_Sk);
+    set(AX,'xlim',[w_Sk(1) w_Sk(end)]);
+    legend([H1;H2],'Intensity','Spectrum')
+    set(get(AX(1),'Ylabel'),'String','arbitrary units')
+    set(get(AX(2),'Ylabel'),'String','[rad]')
+    xlabel('omega [1/fs]')
+    title('Test 4: The pulse with intensity and spectrum as we measured it')
+    
+figure(figNum)
+    figNum = figNum + 1;
+    plot(w_Sk,p_Sk)
+    hold on
+    plot(fit_w_Sk,fit_p_Sk,'LineWidth',2,'Color','red','LineStyle','-.')
+    plot(w_Sk,I_Sk*40-20,'g')
+    hold off
+    xlabel('omega [1/fs]')
+    ylabel('[rad]')
+    legend('Original','Fit range','Intensity')
+    title('Test 4: Visualization of the range we use to find our fit polynomial')
+
+polynomial = polyfit(fit_w_Sk,fit_p_Sk,3);
+O3_fit     = polyval(polynomial,fit_w_Sk);
+    
+figure(figNum)
+    figNum = figNum + 1;
+    plot(fit_w_Sk,fit_p_Sk,'b')
+    hold on
+    plot(fit_w_Sk,fit_I_Sk*40-20,'g')
+    plot(fit_w_Sk,O3_fit,'r')
+    hold off
+    xlabel('omega [1/fs]')
+    ylabel('[rad]')
+    legend('Original','Intensity','O(3) fit')
+    title('Test 4: The resulting least square fit polynomial of degree 3')
+    
+% Find the correct coefficients
+d = polyval(polynomial,w0);
+c = polyval(polyder(polynomial),w0);
+b = polyval(polyder(polyder(polynomial)),w0) / 2;
+a = polyval(polyder(polyder(polyder(polynomial))),w0) / 6;
+
+% One option
+% ord_a = floor(log10(abs(a)));
+% n_a   = 50;
+% ord_b = floor(log10(abs(b)));
+% n_b   = 50;
+% A = sign(a).*logspace(ord_a-1,ord_a+1,n_a);
+% B = sign(b).*logspace(ord_b-1,ord_b+1,n_b);
+% V = zeros(n_b,n_a);
+
+ord_a = floor(log10(abs(a)));
+min_a = a-100*10^(floor(log10(abs(a)))-1);
+max_a = a+100*10^(floor(log10(abs(a)))-1);
+min_a = 0-100*10^(floor(log10(abs(a)))-1);
+max_a = 0+100*10^(floor(log10(abs(a)))-1);
+n_a   = 50;
+ord_b = floor(log10(abs(b)));
+min_b = b-100*10^(floor(log10(abs(b)))-1);
+max_b = b+100*10^(floor(log10(abs(b)))-1);
+n_b   = 50;
+A = linspace(min_a,max_a,n_a);
+B = linspace(min_b,max_b,n_b);
+V = zeros(n_b,n_a);
+
+figure(figNum)
+    figNum = figNum + 1;
+    hold on
+for a2=A
+    for b2=B
+        polyn = [a2 b2 c d];
+        plot(fit_w_Sk,polyval(polyn,fit_w_Sk-w0))
+    end
+end
+    h1 = plot(fit_w_Sk,polyval(polyn,fit_w_Sk-w0));
+    h2 = plot(fit_w_Sk,fit_p_Sk,'r');
+    legend([h1 h2],'Brute force','Original')
+    ylim([min(fit_p_Sk) max(fit_p_Sk)])
+    xlabel('omega [1/fs]')
+    ylabel('[rad]')
+    title('View on the function bulk we run through compared to the original')
+    hold off
+    
+figure(figNum)
+    figNum = figNum + 1;
+    hold on
+for a2=A
+    for b2=B
+        polyn = [a2 b2 c d];
+        plot(fit_w_Sk,fit_p_Sk-polyval(polyn,fit_w_Sk-w0))
+    end
+end
+    h1 = plot(fit_w_Sk,fit_p_Sk-polyval(polyn,fit_w_Sk-w0));
+    h2 = plot(fit_w_Sk,zeros(size(fit_w_Sk)),'r');
+    legend([h1 h2],'Brute force','Zero line')
+    xlabel('omega [1/fs]')
+    ylabel('[rad]')
+    title('View on the differences we get when subtracting the estimate from the original')
+    hold off
+    
+N = 1;
+for a2=A
+    for b2=B
+        polyn = [a2 b2 c d];
+        % Calculate the value
+        val = compensation_minFuncForBruteForce_no_global2(polyn,w0,w,I,p,Int_F);
+        V(N) = val;
         if mod(N,1000)==0
             fprintf('N: %d\n',N)
         end
@@ -177,43 +319,14 @@ figure(figNum)
     h = surf(A,B,V);
     % Necessary, or the lines of the grid will overlay the colors
     set(h,'LineStyle','none')
+    title('Test 4: Calcultated values of our minimization function 2D case')
+   
     
-    
-    
 
 
 
 
 
-
-
-
-
-
-% fprintf('------> 1D\n')
-% % Reserve arrays for storage
-% min_a = 10000;
-% max_a = 100000;
-% n_a   = 1000;
-% A = linspace(min_a,max_a,n_a);
-% V = zeros(1,n_a);
-% 
-% % Enter a loop to brute force the solutions
-% N = 1;
-% for a=A
-%     poly = [a 0 0];
-%     % Calculate the value
-%     val = compensation_minFuncForBruteForce_no_global(poly,w,I,p,Int_F);
-%     V(N) = val;
-%     if mod(N,1000)==0
-%         fprintf('N: %d\n',N)
-%     end
-%     N = N + 1;
-% end
-% 
-% figure(figNum)
-%     figNum = figNum + 1;
-%     plot(V)
     
     
     
